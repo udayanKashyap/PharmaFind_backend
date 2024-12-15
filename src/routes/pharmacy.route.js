cd47788942d13dad9dbc917eb1ba5b346fb541d6
alphacrat <bismoydas333@gmail.com>
2024-12-16 02:25:00 +0530

    registration pharma

--- src/routes/pharmacy.route.js
+++ src/routes/pharmacy.route.js
@@ -3,16 +3,21 @@ const { getMedicines } = require("../controllers/pharmacy.controller");
 const {
     registerPharmacy,
     getAllMedicine,
+    loginPharmacy,
+    getPharmacyDetails
 } = require("../controllers/offlinePharmacy.controller");
 const { addMedicine, getInventory, updateInventory } = require("../controllers/inventory.controller")
 const checkForbiddenMed = require("../middleware/forbiddenMedFilter.middleware");
+const { authenticatePharmacy } = require("../middleware/authenticatePharmacy.middleware");
 const router = express.Router();
 
 router.get("/all", getMedicines);
 router.post("/registerPharma", registerPharmacy);
+router.post("/login", loginPharmacy);
+router.get("/me", authenticatePharmacy, getPharmacyDetails);
 
-router.post("/inventory", addMedicine);
-router.get("/inventory", getInventory);
-router.post("/updateInventory", updateInventory);
+router.post("/inventory", authenticatePharmacy, addMedicine);
+router.get("/inventory", authenticatePharmacy, getInventory);
+router.post("/updateInventory", authenticatePharmacy, updateInventory);
 
 module.exports = router;
